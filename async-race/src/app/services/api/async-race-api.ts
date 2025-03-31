e20c4b387fef24f42ca841e6fb3ab3736a0ea0b0
Ksenia Gorina <fffound27@gmail.com>
2025-03-31 18:02:58 +0200

    feat: add create-, deleteCar functions, fix getCars to accept page

--- async-race/src/app/services/api/async-race-api.ts
+++ async-race/src/app/services/api/async-race-api.ts
@@ -4,13 +4,23 @@ const PATH = {
   GARAGE: '/garage',
   ENGINE: '/engine',
   WINNERS: '/winners',
-};
+} as const;
 
-export async function getCars(): Promise<void> {
-  const response = await fetch(`${BASE_URL}${PATH.GARAGE}`);
-  const data: unknown = await response.json();
+const CARS_PER_PAGE = 7;
+const FIRST_PAGE = 1;
 
-  console.log(data);
+export async function getCars(page = FIRST_PAGE): Promise<void> {
+  const query = new URLSearchParams();
+
+  query.append('_page', page.toString());
+  query.append('_limit', CARS_PER_PAGE.toString());
+
+  const response = await fetch(`${BASE_URL}${PATH.GARAGE}?${query.toString()}`);
+
+  const cars: unknown = await response.json();
+  const totalCount = response.headers.get('X-Total-Count');
+
+  console.log(cars, totalCount);
 }
 
 export async function getCar(id: number): Promise<void> {
@@ -18,6 +28,20 @@ export async function getCar(id: number): Promise<void> {
   const data: unknown = await response.json();
 
   console.log(data);
+}
+
+export async function createCar(name: string, color: string): Promise<unknown> {
+  const response = await fetch(`${BASE_URL}${PATH.GARAGE}`, {
+    method: 'POST',
+    body: JSON.stringify({ name, color }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  return response.json();
+}
+
+export async function deleteCar(id: number): Promise<unknown> {
+  return fetch(`${BASE_URL}${PATH.GARAGE}/${String(id)}`, { method: 'DELETE' });
 }
 
 export async function getWinners(): Promise<void> {
