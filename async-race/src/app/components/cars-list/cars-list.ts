eb4b5f295674ff85a8b8e6bc52e2332beb3d35c7
Ksenia Gorina <fffound27@gmail.com>
2025-04-01 00:15:52 +0200

    refactor: rename car item component

--- async-race/src/app/components/cars-list/cars-list.ts
+++ async-race/src/app/components/cars-list/cars-list.ts
@@ -1,6 +1,6 @@
 import { ul } from '~/app/utils/create-element';
 
-import { createCarItem } from './car-track/car-track';
+import { createCarItem } from './car-item/car-item';
 
 export function createCarsList(): HTMLUListElement {
   const carsList = ul({}, [createCarItem(), createCarItem()]);
